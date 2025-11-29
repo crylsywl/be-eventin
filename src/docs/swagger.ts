553af6f084540eca1c0186d533aa9567dcd05673
crylsywl <crylsyawal123@gmail,com>
2025-11-29 13:31:39 +0700

    Production

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -12,7 +12,7 @@ const doc = {
       description: "Local Server",
     },
     {
-      url: "https://back-end-acara.vercel.app/api",
+      url: "https://be-eventin.vercel.app/api",
       description: "Deploy Server",
     },
   ],
